24e252b746a088a144207d4d3aab4ccfdb2227b9
沈亮 <1797324875@qq.com>
2025-04-20 20:12:49 +0800

    Vertex Buffer Layout

--- SLEngine/src/SLEngine/Application.cpp
+++ SLEngine/src/SLEngine/Application.cpp
@@ -12,6 +12,27 @@ namespace SLEngine
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case SLEngine::ShaderDataType::Float:    return GL_FLOAT;
+		case SLEngine::ShaderDataType::Float2:   return GL_FLOAT;
+		case SLEngine::ShaderDataType::Float3:   return GL_FLOAT;
+		case SLEngine::ShaderDataType::Float4:   return GL_FLOAT;
+		case SLEngine::ShaderDataType::Mat3:     return GL_FLOAT;
+		case SLEngine::ShaderDataType::Mat4:     return GL_FLOAT;
+		case SLEngine::ShaderDataType::Int:      return GL_INT;
+		case SLEngine::ShaderDataType::Int2:     return GL_INT;
+		case SLEngine::ShaderDataType::Int3:     return GL_INT;
+		case SLEngine::ShaderDataType::Int4:     return GL_INT;
+		case SLEngine::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		SL_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		SL_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -28,17 +49,38 @@ namespace SLEngine
 		glBindVertexArray(m_VertexArray);
 
 		// 顶点坐标
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			// 位置             // 颜色
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		// 顶点缓冲对象
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
 		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 		
 		// 顶点索引缓冲对象
 		uint32_t indices[3] = { 0, 1, 2 };
@@ -49,12 +91,15 @@ namespace SLEngine
  			#version 330 core
  			
  			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
  
  			out vec3 v_Position;
+			out vec4 v_Color;
  
  			void main()
  			{
  				v_Position = a_Position;
+				v_Color = a_Color;	
  				gl_Position = vec4(a_Position, 1.0);	
  			}
  		)";
@@ -65,10 +110,11 @@ namespace SLEngine
  			layout(location = 0) out vec4 color;
  
  			in vec3 v_Position;
+			in vec4 v_Color;
  
  			void main()
  			{
- 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
  			}
  		)";
 
