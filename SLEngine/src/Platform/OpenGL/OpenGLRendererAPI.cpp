2f676d5c529995d74b40754e5a42776999f65a38
BlueMoonsl <157781741+BlueMoonsl@users.noreply.github.com>
2025-05-02 13:48:23 +0800

    Single Shader

--- SLEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ SLEngine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -30,6 +30,7 @@ namespace SLEngine {
     void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
     {
         glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
 }
