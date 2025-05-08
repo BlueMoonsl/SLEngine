eab754e4f9b17fa529feddae3f643959a842fbe7
沈亮 <1797324875@qq.com>
2025-05-08 21:23:17 +0800

    Theme and Font

--- SLEngine/src/SLEngine/Core/Window.h
+++ SLEngine/src/SLEngine/Core/Window.h
@@ -13,8 +13,8 @@ namespace SLEngine {
         uint32_t Height;
 
         WindowProps(const std::string& title = "SLEngine",
-            uint32_t width = 1280,
-            uint32_t height = 720)
+			uint32_t width = 1600,
+			uint32_t height = 900)
             : Title(title), Width(width), Height(height) {
         }
     };
